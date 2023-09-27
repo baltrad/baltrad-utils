76a853c953c47fe1c1d989d71ecb83f2c3fb2e2a
Anders Henja <anders.henja@gmail.com>
2023-09-27 17:50:51 +0200

    Added misc scripts

---
+++ misc/build_github_doc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+
+PROJECT_ROOT=$(dirname $(dirname $(readlink -f $0)))
+
+# First setup environment
+sudo apt-get update
+sudo apt-get -y install git rsync python3-sphinx python3-pycryptodome python3-daemon
+
+git config --local user.email "baltrad@users.noreply.github.com"
+git config --local user.name "Baltrad GitHub Action"
+
+cd "$PROJECT_ROOT/doc"
+
+make github

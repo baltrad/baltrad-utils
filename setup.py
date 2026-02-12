400c6623f63076bd7da5f42d43a286863aaa101e
Anders Henja <anders@baltrad.eu>
2026-02-12 15:26:31 +0100

    Handle removal of pkg_resources

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+from setuptools import setup, find_namespace_packages
 
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="baltradutils",
+    version="0.1",
+    packages=find_namespace_packages(where="src", include=["baltradutils*"]),
+    package_dir={"": "src"},
+    namespace_packages=["baltradutils"],
+    package_data={"": ["*.sql", "*.cfg"]},
+    install_requires=[], 
+    zip_safe=False,
+)
--- src/baltradutils/__init__.py
+++ src/baltradutils/__init__.py
@@ -15,3 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with baltrad-utils.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
